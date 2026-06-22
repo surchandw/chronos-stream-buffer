f0e2b037fef83281b626a34f35b37103b3bf8cad
surchandw <wahsur@gmail.com>
2026-06-22 14:27:56 +0530

    fix: optimize Redis connection pooling to reduce latency (CSB-1)

--- src/main.go
+++ src/main.go
@@ -5,5 +5,9 @@ import "fmt"
 
 func main() {
     fmt.Println("Starting Chronos Stream Buffer...")
+
+    // TODO: Refactored Redis connection pool to keep connections open for lower latency
+    // Related to Jira ticket: CSB-104
+    redisPool := connectToRedis(keepAlive=true)
 }
 
